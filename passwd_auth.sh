dfeec38bf4dbd16c36c743736c7e5d521bec9805
bill.zr <bill.zr@alibaba-inc.com>
2019-04-02 21:32:45 +0800

    fix password_auth script

--- passwd_auth.sh
+++ passwd_auth.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 echo "auth-user-pass-verify /etc/openvpn/checkpsw.sh via-env
-username-as-common-name" >> /etc/openvpn/server.conf
+username-as-common-name
+script-security 3 " >> /etc/openvpn/server.conf
 
 cp ./checkpsw.sh /etc/openvpn/checkpsw.sh
 
 chmod 755 /etc/openvpn/checkpsw.sh
 
 touch /etc/openvpn/psw-file
-echo "user1 pass1 # 用户名 密码以空格隔开
+echo "user1 pass1
 user2 pass2" >> /etc/openvpn/psw-file
 
-chmod 400 /etc/openvpn/psw-file
+chmod 444 /etc/openvpn/psw-file
 
 # restart openvpn
 if pgrep systemd-journal; then
